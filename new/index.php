8ada62419ed3f4717e18603999de4c3e3c9efc7a
Michał Terczyński <mterczynski271@gmail.com>
2017-11-01 19:59:25 +0100

    added routing for creating articles

--- new/index.php
+++ new/index.php
@@ -12,16 +12,16 @@
     <title>CMS</title>
 
     <!-- Tether: -->
-    <script src="libs/tether.js"></script>
+    <script src="../libs/tether.js"></script>
     <!-- jQuery: -->
-    <script src="libs/jquery-3.2.1.min.js"></script>
+    <script src="../libs/jquery-3.2.1.min.js"></script>
     <!-- Bootstrap: -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     <!-- Font awesome: -->  
-    <script src="libs/fontAwesome.js"></script>
+    <script src="../libs/fontAwesome.js"></script>
     <!-- Own styles: -->
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="../main.css">
 </head>
 <body>
     <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
@@ -33,10 +33,10 @@
         <div class="collapse navbar-collapse" id="navbarsExampleDefault">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Articles</a>
+                    <a class="nav-link" href="../">Articles</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">My profile</a>
+                    <a class="nav-link" href="../">My profile</a>
                 </li>
             </ul>
             <ul class="navbar-nav ml-auto">
@@ -55,8 +55,6 @@
                 <?php
                     if(isset($_SESSION['user'])) { 
                         echo '<li class="nav-item"><a href="server/logOut.php" class="nav-link logOutLink">Log out</a></li>';
-                    } else {
-
                     }
                 ?> 
             </ul>
@@ -65,21 +63,8 @@
 
    <div class="jumbotron">
         <div class="container">
-            <h1 class="display-3">Articles</h1>
-            <p>
-                Here you can display list of articles. You can also add your own article<br>
-                <?php
-                    if(!isset($_SESSION['user'])) { 
-                        echo '<b>You need to be logged in to add an article.</b>';
-                    } 
-                ?> 
-                
-            </p>
-            <?php
-                if(isset($_SESSION['user'])) { 
-                    echo '<a class="btn btn-success btn-lg" href="./new" role="button">Create article</a>';
-                } 
-            ?> 
+            <h1 class="display-3">Create article</h1>
+            <a class="btn btn-success btn-lg" href="#" role="button">Submit</a></p>
         </div>
    </div>
 </body>
